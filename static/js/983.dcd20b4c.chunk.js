7acc79aa0010adb90416944e318bda966e218f6b
serhii-vasylenko <113785494+serhii-vasylenko@users.noreply.github.com>
2023-05-08 17:13:12 +0000

    Deploying to gh-pages from @ serhii-vasylenko/goit-react-hw-test@5ff2df4793ed5b1fea55d7e477d972d1bfb9e87e 🚀

---
+++ static/js/983.dcd20b4c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_test=self.webpackChunkgoit_react_hw_test||[]).push([[983],{3983:function(e,t,s){s.r(t);var r=s(1087),c=s(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("h2",{children:["Press to see all",(0,c.jsx)(r.rU,{to:"/tweets",className:"home-tweets",children:" TWEETS"})]})})}}}]);
+//# sourceMappingURL=983.dcd20b4c.chunk.js.map
